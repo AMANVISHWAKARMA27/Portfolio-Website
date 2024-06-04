311ce9b8b2e433078e2b1289ed1997d28b4e60dc
aman vsihwakarma <vishwakarmarambhawan576@gmail.com>
2024-06-05 00:04:30 +0530

    hover effect in cards

--- Frontend/portfolio/src/components/CardHolder.jsx
+++ Frontend/portfolio/src/components/CardHolder.jsx
@@ -7,7 +7,7 @@ function CardHolder({ title, langCard }) {
     return (
         <div className='flex flex-col justify-evenly items-start pl-[20px] min-h-[120px] w-[100%] bg-[#46464649] rounded-lg p-2 shadow-lg shadow-[#000000b0] mb-[50px] flex-wrap'>
             <div className='flex flex-col h-[50px] w-[100%]'>
-                <div>
+                <div className="mb-2"> {/* Add margin bottom here */}
                     <h1 className='text-[#6666ff] text-[25px] font-thin'>{title}</h1>
                 </div>
                 <div className='h-[1px] w-[97%] border border-[#ffffff41]'></div>
